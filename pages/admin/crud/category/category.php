c6145364784e5b16e27eede7e394ca3c3b60ee88
Kevin Benedict <kevinbenedict27@gmail.com>
2023-05-29 15:26:15 +0700

    update category

---
+++ pages/admin/crud/category/category.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- My Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <!-- My Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <!-- My Feather Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- My Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,800;1,100;1,200;1,300;1,400;1,600;1,800&display=swap" rel="stylesheet">
+    
+    <title>Category Page | I-News</title>
+</head>
+<body>
+    <h1>Category</h1>
+    <div>
+        <?php 
+            include "../../../../connection\connection.php";
+
+            $data=mysqli_query($con, "SELECT * FROM categories");
+            
+            while($d=mysqli_fetch_array($data)) {
+            ?>
+                <span>
+                    <?php echo $d['name_category']
+                     ?>
+                </span>
+            <?php               
+            }
+        ?> 
+    </div>
+</body>
+</html>
